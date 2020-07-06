a817c9e414b38ef6a2f38fc415ff2a2b677915e1
Chelsea Wetzel <chelswetz@gmail.com>
2020-07-05 20:11:49 -0400

    knapsack problems

---
+++ first_pass_solution.py
@@ -0,0 +1,38 @@
+# Naive
+    # a simple solution that does not make any attempt to optimize performace or memory usage
+# Brute-force
+    # try all possible solutions to find the best one
+# Greedy
+    # at one or more points, this algorithm makes the "best" choice, similar to what a person might do
+
+'''
+The idea is that we have a knapsack with 
+a size or weight limit that contraints
+how many things we can put inside
+
+We have lots of items we want to put into 
+the knapsack. However, they won't all fit.
+Choosing the optimal combination of items 
+that meets size or weight contraints while
+maximizing value might seem doable with a 
+small example, but this becomes a difficult 
+problem very quickly
+'''
+
+# Naive Solution
+# sort items by value - highest valued item first
+# put most valuable items in knapsack until full
+# return knapsack
+
+
+
+# Brute-force Solution
+# go through list of items and make new list with all possible combos of items
+# go through each combination and return the combo with the highest value 
+
+
+
+# Greedy Solution
+# calculate efficiency of each item
+# now sort by eddiciency and add the most efficient items first
+
